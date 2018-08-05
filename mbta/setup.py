a7cd206adcfce82a77e44f1edf10c2ca5a346aa0
Jared Stufft <jared@stufft.us>
2018-08-05 09:07:39 -0400

    adding code for package distribution

---
+++ mbta/setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='mbta',
+    version='0.0.1',
+    author='Jared Stufft',
+    author_email='jared@stufft.us',
+    description='A simple, high-level Python wrapper for the MBTA API',
+    long_description=long_description,
+    url='https://github.com/JaredStufft/mbta',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Development Status :: 3 - Alpha'
+    )
+)
