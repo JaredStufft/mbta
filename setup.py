c20beb52c2e5cf5506cc1007a747dd719b21e0fd
Jared Stufft <jared@stufft.us>
2019-03-03 16:42:22 -0500

    version upgrade

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='mbta',
-    version='0.0.5',
+    version='0.0.6',
     author='Jared Stufft',
     author_email='jared@stufft.us',
     description='A simple, high-level Python wrapper for the MBTA API',
